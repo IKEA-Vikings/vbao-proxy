1f8050b7c277328cfb315d58649abcc6a12c9712
vbaobao <vbao.main@gmail.com>
2021-03-19 01:39:50 -0700

    Attempt at rendering services

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      id: document.location.href.split('/')[3]
+    };
   }
-  
+
+  componentDidMount() {
+    axios.get(`http://localhost:3002/api/sizes/${this.state.id}`)
+      .then((res) => console.log('Successfully found item.', res))
+      .catch((err) => console.error('GET FAILED', err));
+  }
+
   render() {
     return (<h1>Hello</h1>);
   }
