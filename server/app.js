75916465cd3e7bc269ccc681f0ef94629aa8351f
vbaobao <vbao.main@gmail.com>
2021-03-20 17:47:05 -0700

    Improved app.get

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const axios = require('axios');
+const path = require('path');
 
-app.use('/:id', express.static('public'));
+app.use(express.static(path.resolve('public')));
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
 
 app.get(`/api/sizes/:id`, (req, res) => {
   axios.get(`http://localhost:3002/api/sizes/${req.params.id}`)
