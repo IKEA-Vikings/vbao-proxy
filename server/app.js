248893b472d35400f40e6d461c46bdc84796a467
vbaobao <vbao.main@gmail.com>
2021-03-22 20:10:47 -0700

    Removed unneeded GET requests in server

--- server/app.js
+++ server/app.js
@@ -21,33 +21,6 @@ app.get(`/api/reviews/:id/details`, (req, res) => {
       .catch((err) => console.error('GET REVIEWS FAILED: ', err));
 });
 
-app.get(`/api/sizes/:id`, (req, res) => {
-  console.log('GET SIZES');
-  axios.get(`http://localhost:3002/api/sizes/${req.params.id}`)
-      .then((result) => {
-        res.send(result.data);
-      })
-      .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
-});
-
-// app.get(`/api/product/:id`, (req, res) => {
-//   console.log('GET ABOUT');
-//   axios.get(`http://localhost:3003/api/product/${req.params.id}`)
-//       .then((result) => {
-//         res.send(result.data);
-//       })
-//       .catch((err) => console.error('GET ABOUT FAILED: ', err));
-// });
-
-// app.get(`/api/images/type/size/:id`, (req, res) => {
-//   console.log('GET IMAGES');
-//   axios.get(`http://localhost:3004/api/images/type/size/${req.params.id}`)
-//       .then((result) => {
-//         res.send(result.data);
-//       })
-//       .catch((err) => console.error('GET IMAGES FAILED: ', err));
-// });
-
 app.listen(PORT, () => {
   console.log(`Proxy is listening at http://localhost:${PORT}`);
 });
