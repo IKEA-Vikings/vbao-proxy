c46e6cbc1ceeb648307571c8276716cce2923d6d
vbaobao <vbao.main@gmail.com>
2021-04-04 14:49:20 -0700

    Write in footer contents and base elements

--- client/Footer/Footer.jsx
+++ client/Footer/Footer.jsx
@@ -4,11 +4,64 @@ import styled from 'styled-components';
 function Footer(props) {
   return (
     <div>
-      <div>Join IKEA</div>
-      <div>Help</div>
-      <div>Shop & Learn</div>
-      <div>About Ikea</div>
-      <div>Legal</div>
+      <div>
+        <h1>Join IKEA Family</h1>
+        <section>
+          Bring your ideas to life with special discounts, inspiration, and lots of good things in store. It's all free. See more.
+        </section>
+        <button>Join or log in</button>
+      </div>
+      <div>
+        <h1>Help</h1>
+        <ul>
+          <li><a href="#">Customer Service</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">My Orders</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Product Recalls</a></li>
+          <li><a href="#">Return Policy</a></li>
+          <li><a href="#">Warranties</a></li>
+          <li><a href="#">Feedback</a></li>
+        </ul>
+      </div>
+      <div>
+        <h1>Shop & Learn</h1>
+        <ul>
+          <li><a href="#">Find a Location</a></li>
+          <li><a href="#">IKEA Services</a></li>
+          <li><a href="#">IKEA Catalogue & Brochures</a></li>
+          <li><a href="#">IKEA Family</a></li>
+          <li><a href="#">IKEA Planning Tools</a></li>
+          <li><a href="#">Buying Guides</a></li>
+          <li><a href="#">Gift Cards</a></li>
+          <li><a href="#">Gift Registry</a></li>
+          <li><a href="#">IKEA Credit Card Management</a></li>
+          <li><a href="#">IKEA Credit Cards</a></li>
+          <li><a href="#">Explore the New IKEA App</a></li>
+        </ul>
+      </div>
+      <div>
+        <h1>About IKEA</h1>
+        <ul>
+          <li><a href="#">This is IKEA</a></li>
+          <li><a href="#">Careers</a></li>
+          <li><a href="#">Newsroom</a></li>
+          <li><a href="#">Life at Home</a></li>
+          <li><a href="#">A Sustainable Everyday</a></li>
+          <li><a href="#">IKEA Foundation</a></li>
+          <li><a href="#">Creating Safer Homes Together</a></li>
+        </ul>
+      </div>
+      <div>
+        <h1>Legal</h1>
+        <ul>
+          <li><a href="#">Privacy & Security</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms and Conditions</a></li>
+          <li><a href="#">IKEA Children's Product Registration</a></li>
+          <li><a href="#">IKEA SMÅLAND Privacy notice</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
