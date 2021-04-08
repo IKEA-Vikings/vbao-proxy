7e016e9054624c924ab580c1fa784ca72bf4d74e
vbaobao <vbao.main@gmail.com>
2021-04-07 23:04:21 -0700

    style header component elements

---
+++ client/Header/styles_header.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import {FONT, GRAY, WHITE, EGGSHELL, FONT_MED, FONT_LG} from '../style-constants.js';
+
+export const Bar = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  font-family: ${FONT};
+`;
+
+export const Logo = styled.h1`
+  font-size: 2rem;
+`;
