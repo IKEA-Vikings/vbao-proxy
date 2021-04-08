0b5ae5e409900dcd5e3258eb3d068e3d6164a5fb
vbaobao <vbao.main@gmail.com>
2021-04-07 23:04:11 -0700

    add styled component elements

--- client/Header/Header.jsx
+++ client/Header/Header.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import Search from './Search.jsx';
 import Nav from './Nav.jsx';
+import {Bar, Logo} from './styles_header.js';
 
 function Header(props) {
   return (
-    <div>
-      <div>
-        <img src="https://www.ikea.com/us/en/static/ikea-logo.f88b07ceb5a8c356b7a0fdcc9a563d63.svg"/>
-      </div>
+    <Bar>
+      <Logo>IKEA</Logo>
       <Search />
       <Nav />
-    </div>
+    </Bar>
   );
 }
 
