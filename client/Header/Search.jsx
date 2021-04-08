3dba95658550f725841e261b526961eb89b8b693
vbaobao <vbao.main@gmail.com>
2021-04-08 01:29:46 -0700

    initial search bar styling

--- client/Header/Search.jsx
+++ client/Header/Search.jsx
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faSearch, faCamera} from '@fortawesome/free-solid-svg-icons';
+import {Form, Bar} from './styles_search.js';
 
 function Search(props) {
   return (
     <div>
       <span><FontAwesomeIcon icon={faSearch} size="lg"/></span>
-      <form>
-        <input type="text" name="query" value="What are you looking for?"/>
-      </form>
+      <Form>
+        <Bar type="text" name="query" value="What are you looking for?"/>
+      </Form>
       <button><FontAwesomeIcon icon={faCamera} size="lg"/></button>
     </div>
   );
---
+++ client/Header/styles_search.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import {FONT, GRAY, WHITE, EGGSHELL, FONT_MED, FONT_LG} from '../style-constants.js';
+
+export const Form = styled.form`
+  width: 40%;
+`;
+
+export const Bar = styled.input`
+  border: none;
+  border-radius: 50px;
+  background-color: ${EGGSHELL};
+`;
