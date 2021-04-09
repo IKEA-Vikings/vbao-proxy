0d03d10ba7b231e7971bc10671c00c64758965a2
vbaobao <vbao.main@gmail.com>
2021-04-08 18:21:44 -0700

    style search bar

--- client/Header/Search.jsx
+++ client/Header/Search.jsx
@@ -3,17 +3,19 @@ import styled from 'styled-components';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faSearch, faCamera} from '@fortawesome/free-solid-svg-icons';
-import {Form, Bar} from './styles_search.js';
+import {Bar, Find, Button, ButtonWrapper, Input} from './styles_search.js';
 
 function Search(props) {
   return (
-    <div>
-      <span><FontAwesomeIcon icon={faSearch} size="lg"/></span>
-      <Form>
-        <Bar type="text" name="query" value="What are you looking for?"/>
-      </Form>
-      <button><FontAwesomeIcon icon={faCamera} size="lg"/></button>
-    </div>
+    <Bar>
+      <Find><FontAwesomeIcon icon={faSearch}/></Find>
+      <form>
+        <Input type="text" name="query" value="What are you looking for?"/>
+      </form>
+      <ButtonWrapper>
+        <Button><FontAwesomeIcon icon={faCamera} size="lg"/></Button>
+      </ButtonWrapper>
+    </Bar>
   );
 }
 
--- client/Header/styles_search.js
+++ client/Header/styles_search.js
@@ -1,12 +1,60 @@
 import styled from 'styled-components';
-import {FONT, GRAY, WHITE, EGGSHELL, FONT_MED, FONT_LG} from '../style-constants.js';
+import {FONT, GRAY, WHITE, EGGSHELL, FONT_LG} from '../style-constants.js';
 
-export const Form = styled.form`
-  width: 40%;
-`;
-
-export const Bar = styled.input`
+export const Bar = styled.div`
+  position: relative;
   border: none;
   border-radius: 50px;
   background-color: ${EGGSHELL};
+  width: 50%;
+  height: 50px;
+
+  display: flex;
+  line-height: 50px;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+export const Find = styled.span`
+  margin-left: 20px;
+`;
+
+export const Button = styled.button`
+  border: none;
+  background: none;
+  border-radius: 50px;
+  background-color: ${EGGSHELL};
+  height: 35px;
+  width: 35px;
+  line-height: 35px;
+  text-align: center;
+  vertical-align: middle;
+  padding: 0;
+
+  &:hover {
+    background-color: ${WHITE}
+  }
+`;
+
+export const Input = styled.input`
+  border: none;
+  background: none;
+  margin-left: 10px;
+  font-family: ${FONT};
+  font-size: ${FONT_LG};
+  color: ${GRAY};
+  width: 100%;
+  padding-right: 30px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  background: none;
+  position: absolute;
+  right: 0;
 `;
