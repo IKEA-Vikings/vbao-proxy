024bc94754374fd61210aab3934dc605bcd5e4c6
vbaobao <vbao.main@gmail.com>
2021-04-17 03:31:42 -0700

    include backup localhost endpoints

--- server/app.js
+++ server/app.js
@@ -12,6 +12,15 @@ app.get('/:id', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
+// app.get(`/api/reviews/:id/details`, (req, res) => {
+//   console.log('GET REVIEWS');
+//   axios.get(`http://localhost:3001/api/reviews/${req.params.id}/details`)
+//       .then((result) => {
+//         res.send(result.data);
+//       })
+//       .catch((err) => console.error('GET REVIEWS FAILED: ', err));
+// });
+
 app.get(`/api/reviews/:id/details`, (req, res) => {
   console.log('GET REVIEWS');
   axios.get(`http://100.25.191.161/api/reviews/${req.params.id}/details`)
@@ -30,14 +39,14 @@ app.get(`/api/sizes/:id`, (req, res) => {
       .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
 });
 
-app.get(`/images/org/:id`, (req, res) => {
-  console.log('GET IMAGES');
-  axios.get(`http://54.67.28.46:3004/images/org/${req.params.id}`)
-      .then((result) => {
-        res.send(result.data);
-      })
-      .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
-});
+// app.get(`/api/product/:id`, (req, res) => {
+//   console.log('GET ABOUT');
+//   axios.get(`http://localhost:3003/api/product/${req.params.id}`)
+//       .then((result) => {
+//         res.send(result.data);
+//       })
+//       .catch((err) => console.error('GET ABOUT FAILED: ', err));
+// });
 
 app.get(`/api/product/:id`, (req, res) => {
   console.log('GET ABOUT');
@@ -45,7 +54,25 @@ app.get(`/api/product/:id`, (req, res) => {
       .then((result) => {
         res.send(result.data);
       })
-      .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
+      .catch((err) => console.error('GET ABOUT FAILED: ', err));
+});
+
+app.get(`/images/org/:id`, (req, res) => {
+  console.log('GET IMAGES');
+  axios.get(`http://54.67.28.46:3004/images/org/${req.params.id}`)
+      .then((result) => {
+        res.send(result.data);
+      })
+      .catch((err) => console.error('GET IMAGES FAILED: ', err));
+});
+
+app.get(`/similar-products-by-views/:id`, (req, res) => {
+  console.log('GET OTHERS ALSO LIKED');
+  axios.get(`http://18.222.25.224:3005/similar-products-by-views/${req.params.id}`)
+      .then((result) => {
+        res.send(result.data);
+      })
+      .catch((err) => console.error('GET OTHERS ALSO VIEWED FAILED: ', err));
 });
 
 app.listen(PORT, () => {
